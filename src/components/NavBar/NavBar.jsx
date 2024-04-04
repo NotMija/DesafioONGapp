f8cbbd00b633a79347a6c4c9644e01c304ea81a9
ricardoyp <ricardoyp07@gmail.com>
2024-04-04 22:46:30 +0200

    "Finished NavBar"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -44,23 +44,36 @@ export const NavBar = () => {
     return (
         <AppBar position="static" style={{ backgroundColor: '#364F75', padding: '32px', width: '288px' }}>
             <Toolbar style={{ display: 'flex', flexDirection: 'column', gap: '24px', padding: '24px 8px' }} >
-                {NavBarElements.map((tab) => (
-                    <>
-                        <NavBarButton
-                            key={tab.name}
-                            selectedTab={selectedTab}
-                            tabName={tab.name}
-                            handleClick={tab.subButtons.length === 0 ? handleClick : undefined}
+
+                {NavBarElements.map((tab) => {
+                    const [showSubButtons, setShowSubButtons] = useState(true);
+                    const handleChangeButton = () => {
+                        setShowSubButtons(!showSubButtons);
+                    }
+
+                    return (
+                        <>
+                            <NavBarButton
+                                key={tab.name}
+                                selectedTab={selectedTab}
+                                tabName={tab.name}
+                                handleClick={tab.subButtons.length === 0 ? handleClick : handleChangeButton}
+                                showSubButtons={tab.subButtons.length > 0 ? showSubButtons : undefined}
                             />
-                        {tab.subButtons.length > 0 &&
-                            <div style={{display: 'flex', gap: '8px', flexDirection: 'column', paddingLeft: '32px', justifyContent: 'flex-end', alignItems: 'center', alignSelf: 'stretch'}}>
-                                {tab.subButtons.map((subButton) => (
-                                    <NavBarSubButton key={subButton} tabName={subButton} handleClick={handleClick} selectedTab={selectedTab}/>
-                                ))}
-                            </div>
-                        }
-                    </>
-                ))}
+                            {tab.subButtons.length > 0 && (
+                                <>
+                                    {showSubButtons && (
+                                        <div style={{ display: 'flex', gap: '8px', flexDirection: 'column', paddingLeft: '32px', justifyContent: 'flex-end', alignItems: 'center', alignSelf: 'stretch' }}>
+                                            {tab.subButtons.map((subButton) => (
+                                                <NavBarSubButton key={subButton} tabName={subButton} handleClick={handleClick} selectedTab={selectedTab} />
+                                            ))}
+                                        </div>
+                                    )}
+                                </>
+                            )}
+                        </>
+                    );
+                })}
             </Toolbar>
         </AppBar>
     );
