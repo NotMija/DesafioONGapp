81a94cfbf85a8078b83f3c9d306a9ffcc2490137
cristinamedrom <cristinamedranoroman@gmail.com>
2024-04-08 19:49:53 +0200

    Education LKG y UKG: Faltaría lógica para pasar lista y gráficos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { NavBar } from './components/NavBar/NavBar'
 import { Dashboard } from './screens/Dashboard'
 import { Route, Routes } from 'react-router-dom'
 import { EducationNursery } from './screens/EducationNursery'
+import { EducationLKG } from './screens/EducationLKG'
+import { EducationUKG } from './screens/EducationUKG'
 import { Food } from './screens/Food'
 import { Advocacy } from './screens/Advocacy'
 import { Reports } from './screens/Reports'
@@ -14,6 +16,8 @@ export const App = () => {
       <Route element={<NavBar />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/nursery" element={<EducationNursery />} />
+        <Route path="/LKG" element={<EducationLKG />} />
+        <Route path="/UKG" element={<EducationUKG />} />
         <Route path="/food" element={<Food />} />
         <Route path="/advocacy" element={<Advocacy/>} />
         <Route path="reports" element={<Reports />} />
