2fea34e9c3fc63aa708923713a0ec0c691ee6cc4
Adrián Mijarra <adrian.mijarra@gmail.com>
2024-04-16 18:18:04 +0200

    sigup css

--- src/screens/Reports.jsx
+++ src/screens/Reports.jsx
@@ -1,9 +1,10 @@
 import { TextH2 } from "../components/Text/TextH2";
+import reportIcon from "../img/icon/report.svg"; // Importa el archivo SVG
 
 export const Reports = () => {
     return (
         <div style={{width: '100%', height: '100%', backgroundColor: '#E4EBF7'}}>
-            <TextH2 iconUrl='src/images/icon/ReportsOrange.svg' name='Reports' />
+            <TextH2 iconUrl={reportIcon} name='Reports' /> {/* Pasa la importación como prop iconUrl */}
         </div>
     );
 }
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Button, Card, TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
@@ -14,13 +14,16 @@ export default function SignUp() {
   } = useForm();
 
   // Define the mutation for signing up
-  const { mutate: signupMutation } = useMutation({
+  const { mutate: signupMutation, isLoading } = useMutation({
     mutationKey: "signup",
     mutationFn: async ({ email, password }) => {
       return await createUserWithEmailAndPassword(auth, email, password);
     },
     onSuccess: () => {
-      navigate('/login'); // Redirige al usuario a la página de inicio de sesión
+      navigate("/Dashboard");
+    },
+    onError: (error) => {
+      console.error("Error signing up:", error);
     },
   });
 
@@ -28,39 +31,72 @@ export default function SignUp() {
     signupMutation(data);
   };
 
+  // Función para manejar la navegación a la página principal
+  const goToHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="container mx-auto flex justify-center items-center flex-col min-h-[100vh] gap-8">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="email">
-          Email
-          <input
-            className="input"
-            type="email"
-            name="email"
-            autoComplete="username"
-            {...register("email", { required: true })}
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        backgroundImage: 'url(https://www.meraparivar.org/wp-content/uploads/2022/07/WhatsApp-Image-2024-04-02-at-1.52.28-PM-2.jpeg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+        '&::before': {
+          content: '""',
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        },
+      }}
+    >
+      <Card
+        raised
+        sx={{
+          position: 'relative',
+          zIndex: 2,
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 2,
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <TextField 
+            label="Email" 
+            variant="outlined" 
+            fullWidth 
+            {...register("email", { required: true })} 
+            error={!!errors.email} 
+            helperText={errors.email ? "This field is required" : ""} 
           />
-          {errors.email && <span>This field is required</span>}
-        </label>
-        <label htmlFor="password">
-          Password
-          <input
-            className="input"
-            type="password"
-            name="password"
-            autoComplete="new-password"
-            {...register("password", { required: true })}
+          <TextField 
+            label="Password" 
+            type="password" 
+            variant="outlined" 
+            fullWidth 
+            {...register("password", { required: true })} 
+            error={!!errors.password} 
+            helperText={errors.password ? "This field is required" : ""}
           />
-          {errors.password && <span>This field is required</span>}
-        </label>
-        <button
-          className="button"
-          type="submit"
-          disabled={signupMutation.isLoading}
-        >
-          Sign up
-        </button>
-      </form>
-    </div>
+          <Button type="submit" variant="contained" fullWidth disabled={isLoading}>
+            Sign up
+          </Button>
+        </form>
+        <Button variant="text" onClick={goToHome} fullWidth>
+          Go to Login
+        </Button>
+      </Card>
+    </Box>
   );
 }
